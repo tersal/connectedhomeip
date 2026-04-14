0f83a99bfe8d2ad05abc2526d484ede6d42a7f35
Moises Terrones <moises.tersal@gmail.com>
2026-04-14 00:13:21 -0600

    Add static assert to function

--- src/app/persistence/AttributePersistenceMigration.h
+++ src/app/persistence/AttributePersistenceMigration.h
@@ -36,6 +36,7 @@ struct AttrMigrationData
 template <size_t N>
 constexpr size_t MaxAttrMigrationValueSize(const AttrMigrationData (&attrs)[N])
 {
+    static_assert(N > 0, "Migration attributes array must not be empty");
     size_t maxSize = 0;
     for (size_t i = 0; i < N; i++)
     {
