dfb43fb2f471b4cc6653cd4ab271ab4aace4938a
Moises Terrones <moises.tersal@gmail.com>
2026-04-21 14:03:07 -0600

    Add basic migration implementation from SafeAttributePersistence

--- src/app/clusters/resource-monitoring-server/app_config_dependent_sources.cmake
+++ src/app/clusters/resource-monitoring-server/app_config_dependent_sources.cmake
@@ -19,6 +19,10 @@ TARGET_SOURCES(
   PRIVATE
     "${CLUSTER_DIR}/CodegenIntegration.h"
     "${CLUSTER_DIR}/CodegenIntegration.cpp"
+    "${CLUSTER_DIR}/CodegenResourceMonitoringCluster.cpp"
+    "${CLUSTER_DIR}/CodegenResourceMonitoringCluster.h"
+    "${CLUSTER_DIR}/MigrateResourceMonitoringServerStorage.cpp"
+    "${CLUSTER_DIR}/MigrateResourceMonitoringServerStorage.h"
     "${CLUSTER_DIR}/resource-monitoring-server.h"
 )
 
