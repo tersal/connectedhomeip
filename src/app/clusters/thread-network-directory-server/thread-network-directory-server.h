374e87d0542b530b671fd93711f6de863437430c
Restyled.io <commits@restyled.io>
2026-04-29 21:54:14 +0000

    Restyled by clang-format

--- src/app/clusters/thread-network-directory-server/thread-network-directory-server.h
+++ src/app/clusters/thread-network-directory-server/thread-network-directory-server.h
@@ -16,6 +16,6 @@
  */
 
 #pragma once
-//TODO: This file is used to keep backwards compatibility after CodeDriven migration, should be changed to
-// point to CodegenIntegration or similar after the cluster is properly migrated.
+// TODO: This file is used to keep backwards compatibility after CodeDriven migration, should be changed to
+//  point to CodegenIntegration or similar after the cluster is properly migrated.
 #include "thread-network-directory-server.h"
